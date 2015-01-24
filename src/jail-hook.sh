39c47f14640a5ac49190158e500d2bb6e1b5cfa2
Billy Lamberta <b@lamberta.org>
2015-01-23 21:28:15 -0800

    Debugging jail-hook, seems to work.

--- src/jail-hook.sh
+++ src/jail-hook.sh
@@ -2,14 +2,13 @@
 ## Run the command hook files for jailcreate and jailupdate.
 ## Available environmental vars: JAIL_ID, JAIL_IP, JAIL_TYPE, JAIL_USER
 
-JAIL_TYPE="$1"
-HOOK_NAME="${2%.*}" #remove file extension if given
-HOOK_ENV="$3"
+HOOK_NAME="${1%.*}" #remove file extension if given
+HOOK_ENV="$2"
 APP_ROOT="$(cd $(dirname $(readlink -f $0))/..; pwd)"
 JAIL_CONF_DIR=''
 
-if [ "$HOOK_ENV" != 'host' -o "$HOOK_ENV" != 'jail' ]; then
-  echo "$(basename $0): The hook must be run in either the 'host' or 'jail'." >&2
+if [ "$HOOK_ENV" != 'host' -a "$HOOK_ENV" != 'jail' ]; then
+  echo "$(basename $0): The hook '$HOOK_NAME' must be run in either the 'host' or 'jail' environment." >&2
   exit 1
 fi
 
@@ -18,16 +17,18 @@ fi
 ##
 
 #first check path var
+#can be a jail directory, or a directory containing jail directories
 if [ -n "$SERVERCONF_JAIL_PATH" ]; then
   for dir in $(echo "$SERVERCONF_JAIL_PATH" | tr ':' ' '); do
-    if [ ! -d "$dir" ]; then
-      echo "$(basename $0): In var SERVERCONF_JAIL_PATH, '$dir' must be a directory." >&2
-      exit 1
-    fi
-    if [ -d "$dir/$JAIL_TYPE" ]; then
-      #found jail type
+    if [ -d "$dir" -a "$(basename $dir)" == "$JAIL_TYPE" ]; then
       JAIL_CONF_DIR="$dir"
       break
+    elif [ -d "$dir/$JAIL_TYPE" ]; then
+      JAIL_CONF_DIR="$dir/$JAIL_TYPE"
+      break
+    else
+      echo "$(basename $0): In SERVERCONF_JAIL_PATH, '$dir' must be a directory." >&2
+      exit 1
     fi
   done
 fi
@@ -35,9 +36,9 @@ fi
 #then check app defaults
 cd "$APP_ROOT/jails"
 for dir in $(find . ! -path . -type d -maxdepth 1); do
-  if [ -d "$dir/$JAIL_TYPE" ]; then
+  if [ "$(basename $dir)" == "$JAIL_TYPE" ]; then
     #found jail type
-    JAIL_CONF_DIR="$APP_ROOT/jails/$dir"
+    JAIL_CONF_DIR="$APP_ROOT/jails/$(basename $dir)"
     break
   fi
 done
@@ -58,7 +59,7 @@ HOOK_FILE=$(find "$JAIL_CONF_DIR" -name "$HOOK_NAME*" -type f -maxdepth 1 | head
 ##
 
 if [ -f "$HOOK_FILE" -a "$HOOK_ENV" == 'host' ]; then
-  echo "Running $HOOK_NAME for '$JAIL_TYPE' on $HOOK_ENV ..."
+  echo "Running '$HOOK_NAME' hook for '$JAIL_TYPE' on $HOOK_ENV ..."
   if ! env \
        JAIL_ID="$JAIL_ID" \
        JAIL_IP="$JAIL_IP" \
@@ -67,7 +68,7 @@ if [ -f "$HOOK_FILE" -a "$HOOK_ENV" == 'host' ]; then
        HOOK_NAME="$HOOK_NAME" \
        HOOK_ENV="$HOOK_ENV" \
        JAIL_CONF_DIR="$JAIL_CONF_DIR" \
-       sh -e "$JAIL_CONF_DIR/$HOOK_FILE"; then
+       sh -e "$HOOK_FILE"; then
     echo "Error running '$JAIL_TYPE/$HOOK_NAME', continuing" >&2
   fi
 fi
@@ -84,7 +85,7 @@ if [ -f "$HOOK_FILE" -a "$HOOK_ENV" == 'jail' ]; then
     echo "In '$JAIL_TYPE/$HOOK_NAME', unable to mount $JAIL_CONF_DIR directory within jail, skipping" >&2
 
   else
-    echo "Running $HOOK_NAME for '$JAIL_TYPE' on $HOOK_ENV ..."
+    echo "Running '$HOOK_NAME' hook for '$JAIL_TYPE' in $HOOK_ENV ..."
     if ! env \
          JAIL_ID="$JAIL_ID" \
          JAIL_IP="$JAIL_IP" \
@@ -93,7 +94,7 @@ if [ -f "$HOOK_FILE" -a "$HOOK_ENV" == 'jail' ]; then
          HOOK_NAME="$HOOK_NAME" \
          HOOK_ENV="$HOOK_ENV" \
          JAIL_CONF_DIR="/tmp/$JAIL_TYPE" \
-         jexec "$JAIL_ID" sh -e "/tmp/$JAIL_TYPE/$HOOK_FILE"; then
+         jexec "$JAIL_ID" sh -e "/tmp/$JAIL_TYPE/$(basename $HOOK_FILE)"; then
       echo "Error running '$JAIL_TYPE/$HOOK_NAME', continuing" >&2
     fi
 
