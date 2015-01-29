b9b2327b96d94d96c4a8a2a2063b6bfb865b7e29
Billy Lamberta <b@lamberta.org>
2015-01-29 13:19:22 -0800

    Fix bug in get-jail-conf

--- src/get-jail-conf.sh
+++ src/get-jail-conf.sh
@@ -1,37 +1,60 @@
 #!/bin/sh -e
 
 JAIL_TYPE="$1"
-APP_ROOT="$(cd $(dirname $(readlink -f $0))/..; pwd)"
+JAIL_CONF_DIR=''
+#the default is to throw an error if it can't locate the directory
+ERROR_ON_FAIL=1
+if [ -n "$NO_ERROR_ON_FAIL" ]; then ERROR_ON_FAIL=''; fi
 
-#SERVERCONF_JAIL_PATH is a list of jail dirs, or dir that contains jail dirs
+##
+## the environ var SERVERCONF_JAIL_PATH can be a list of jail dirs,
+## separated by a colon, or of parent dir that contains multiple jail dirs.
+##
+
 if [ -n "$SERVERCONF_JAIL_PATH" ]; then
+
   for dir in $(echo "$SERVERCONF_JAIL_PATH" | tr ':' ' '); do
-    if [ -d "$dir" -a "$(basename $dir)" == "$JAIL_TYPE" ]; then
+
+    if [ ! -d "$dir" ]; then
+      echo "$(basename $0): In SERVERCONF_JAIL_PATH, '$dir' must be a directory." >&2
+      exit 1;
+    fi
+    
+    if [ "$(basename $dir)" == "$JAIL_TYPE" ]; then
       JAIL_CONF_DIR="$dir"
-      break
+      break;
+
     elif [ -d "$dir/$JAIL_TYPE" ]; then
       JAIL_CONF_DIR="$dir/$JAIL_TYPE"
-      break
-    else
-      echo "$(basename $0): In SERVERCONF_JAIL_PATH, '$dir' must be a directory." >&2
-      exit 1
+      break;
     fi
   done
 fi
 
-#if not using SERVERCONF_JAIL_PATH, check app defaults
-cd "$APP_ROOT/jails"
-
-for dir in $(find . ! -path . -type d -maxdepth 1); do
-  if [ "$(basename $dir)" == "$JAIL_TYPE" ]; then
-    JAIL_CONF_DIR="$APP_ROOT/jails/$(basename $dir)"
-    break
-  fi
-done
+##
+## if we haven't found it yet, search app defaults
+##
 
 if [ -z "$JAIL_CONF_DIR" ]; then
-  echo "$(basename $0): Unable to find jail type config directory, aborting." >&2;
+
+  APP_ROOT="$(cd $(dirname $(readlink -f $0))/..; pwd)"
+  cd "$APP_ROOT/jails"
+
+  for dir in $(find . ! -path . -type d -maxdepth 1); do
+    if [ "$(basename $dir)" == "$JAIL_TYPE" ]; then
+      JAIL_CONF_DIR="$APP_ROOT/jails/$(basename $dir)"
+      break;
+    fi
+  done  
+fi
+
+##
+## return dir location or error
+##
+
+if [ -z "$JAIL_CONF_DIR" -a -n "$ERROR_ON_FAIL" ]; then
+  echo "$(basename $0): Unable to find config directory for '$JAIL_TYPE', aborting." >&2;
   exit 1
 else
-	echo "$JAIL_CONF_DIR"
+  echo "$JAIL_CONF_DIR"
 fi
