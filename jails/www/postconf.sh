73e4a04f016cbe9e762c9f8c4df0b7d108aa8b1a
Billy Lamberta <b@lamberta.org>
2015-01-29 15:10:10 -0800

    Removed www jail. Not relevant anymore.

--- jails/www/postconf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-## Used by jail-create, this script is run on the host system after the
-## jail has been created and started. Ran after jail/conf.
-## The default/postconf file is run for all new jails, and before any
-## type-specific jail/postconf script is run.
-## Available environmental vars: JAIL_NAME, JAIL_IP, JAIL_TYPE, JAIL_USER, JAIL_CONF_DIR
-
-#re-mount using updated /etc/fstab
-mount -a
